fb31ffb5c3a5fceaf7465c769e2329890b312670
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-06-06 22:19:22 -0300

    editando

--- index.php
+++ index.php
@@ -2,28 +2,30 @@
 include "cabecalho.php";
 include "banner.php";
 ?>
-    
-    <h2>Melhores avaliações</h2>
-    <div class="lista-filmes">
-        <div class="card-filme">
+<div class="container">
+    <h2 class="display-5">Melhores avaliações</h2>
+
+    <div class="row">
+        <div class="col-3">
             <img src="img/filme1.png">
             <h3>Jurassic Park</h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="card-filme">
+        <div class="col-3">
             <img src="img/filme2.png">
             <h3>Karate Kid Legends</h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="card-filme">
+        <div class="col-3">
             <img src="img/filme3.png">
             <h3>Lilo e Stitch</h3>
             <span>⭐ 10/10</span>
         </div>
-        <div class="card-filme">
+        <div class="col-3">
             <img src="img/filme4.png">
             <h3>Pecadores</h3>
             <span>⭐ 10/10</span>
         </div>
     </div>
-    <?php include "rodape.php"; ?>
+</div>
+<?php include "rodape.php"; ?>
