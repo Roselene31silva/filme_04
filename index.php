b04332dacfc32f9534e869e1edaf3517c3f5d923
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-06-09 22:27:10 -0300

    meu deus

--- index.php
+++ index.php
@@ -7,25 +7,49 @@ include "banner.php";
 
     <div class="row">
         <div class="col-3">
-            <img src="img/filme1.png">
+            <img src="img/filme1.png" class="img-fluid">
             <h3>Jurassic Park</h3>
             <span>⭐ 10/10</span>
         </div>
         <div class="col-3">
-            <img src="img/filme2.png">
+            <img src="img/filme2.png" class="img-fluid">
             <h3>Karate Kid Legends</h3>
             <span>⭐ 10/10</span>
         </div>
         <div class="col-3">
-            <img src="img/filme3.png">
+            <img src="img/filme3.png" class="img-fluid">
             <h3>Lilo e Stitch</h3>
             <span>⭐ 10/10</span>
         </div>
         <div class="col-3">
-            <img src="img/filme4.png">
+            <img src="img/filme4.png" class="img-fluid">
             <h3>Pecadores</h3>
             <span>⭐ 10/10</span>
         </div>
     </div>
+    <div class="row">
+        <div class="col-6">
+            <img src="img/banner.jpg" class="img-fluid">
+        </div>
+        <div class="col align-content-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate explicabo autem nobis rem iusto ullam voluptatum 
+            harum voluptatem iste facilis unde fuga, sequi ut laborum hic 
+            consequatur possimus magni maiores.</p>
+            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER AGORA</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6 align-content-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Cupiditate explicabo autem nobis rem iusto ullam voluptatum 
+            harum voluptatem iste facilis unde fuga, sequi ut laborum hic 
+            consequatur possimus magni maiores.</p>
+            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER AGORA</a>
+        </div>
+        <div class="col-6">
+        <img src="img/banner.jpg" class="img-fluid">
+        </div>
+    </div> 
 </div>
 <?php include "rodape.php"; ?>
--- listadefilmes.php
+++ listadefilmes.php
@@ -2,88 +2,103 @@
 include "cabecalho.php";
 include "banner.php";
 ?>
+<div class="container">
+    <h2>Todos os filmes</h2>
+    <div class="row">
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme1.png" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Jurrassic Park</h5>
+                    <p class="card-text">⭐ 10/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme2.png" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">karate Kid Legends</h5>
+                    <p class="card-text">⭐ 5/10</p>
+                    <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/filme3.png" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Lilo e Stitch</h5>
+                    <p class="card-text">⭐10/10
+                        <a href="umfilme.php" class="btn btn-primary">Veja detalhes</a>
 
-<h2>Todos os filmes</h2>
-<div class="lista-filmes">
-        <div class="card-filme">
-            <img src="img/filme1.png">
-            <a href="umfilme.php"><h3>Jurassic Park</h3></a>
-            <span>⭐ 10/10</span>
+                </div>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme4.png">
+                <h3>Pecadores</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme1.png">
+                <h3>Jurassic Park</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme2.png">
+                <h3>Karate Kid Legends</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme3.png">
+                <h3>Lilo e Stitch</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme4.png">
+                <h3>Pecadores</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme1.png">
+                <h3>Jurassic Park</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme2.png">
+                <h3>Karate Kid Legends</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme3.png">
+                <h3>Lilo e Stitch</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme4.png">
+                <h3>Pecadores</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme1.png">
+                <h3>Jurassic Park</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme2.png">
+                <h3>Karate Kid Legends</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme3.png">
+                <h3>Lilo e Stitch</h3>
+                <span>⭐ 10/10</span>
+            </div>
+            <div class="card-filme">
+                <img src="img/filme4.png">
+                <h3>Pecadores</h3>
+                <span>⭐ 10/10</span>
+            </div>
         </div>
-        <div class="card-filme">
-            <img src="img/filme2.png">
-            <h3>Karate Kid Legends</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme3.png">
-            <h3>Lilo e Stitch</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme4.png">
-            <h3>Pecadores</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme1.png">
-            <h3>Jurassic Park</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme2.png">
-            <h3>Karate Kid Legends</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme3.png">
-            <h3>Lilo e Stitch</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme4.png">
-            <h3>Pecadores</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme1.png">
-            <h3>Jurassic Park</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme2.png">
-            <h3>Karate Kid Legends</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme3.png">
-            <h3>Lilo e Stitch</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme4.png">
-            <h3>Pecadores</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme1.png">
-            <h3>Jurassic Park</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme2.png">
-            <h3>Karate Kid Legends</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme3.png">
-            <h3>Lilo e Stitch</h3>
-            <span>⭐ 10/10</span>
-        </div>
-        <div class="card-filme">
-            <img src="img/filme4.png">
-            <h3>Pecadores</h3>
-            <span>⭐ 10/10</span>
-        </div>
-</div>
-<?php include "rodape.php"; ?>
+        <?php include "rodape.php"; ?>
